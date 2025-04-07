ea64f5abb8eff37973caf575108b9d8363d49f5d
Nicolas-Thoeni <nicolas.castillothoeni@insel.ch>
2025-04-07 14:27:43 +0200

    feat: finsished questionnaire part one

--- 4_Umsetzung/Sourcecode/frontend/src/app/components/sections/questionnaire-part-one/questionnaire-part-one.component.ts
+++ 4_Umsetzung/Sourcecode/frontend/src/app/components/sections/questionnaire-part-one/questionnaire-part-one.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
-  selector: 'app-questionnaire-part-one',
-  imports: [],
-  templateUrl: './questionnaire-part-one.component.html',
-  styleUrl: './questionnaire-part-one.component.css'
+    selector: 'app-questionnaire-part-one',
+    imports: [ReactiveFormsModule],
+    templateUrl: './questionnaire-part-one.component.html',
+    styleUrl: './questionnaire-part-one.component.css'
 })
 export class QuestionnairePartOneComponent {
-
+    personForm = new FormGroup({
+        name: new FormControl('')
+    })
 }
